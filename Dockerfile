cbaf31c0fed4e58359cba88106fd2e982adac747
Zachary McGuire <zachary_mcguire@outlook.com>
2020-06-27 22:36:18 +1000

    setup mongoDB

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,11 @@ FROM python:3.8-alpine
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-ENV FLASK_APP shortnr
 ENV FLASK_RUN_HOST 0.0.0.0
-ENV FLASK_ENV=development
-ENV SECRET_KEY=change_me
 
 RUN apk add --no-cache gcc musl-dev linux-headers
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY . usr/src/shortnr/
+WORKDIR usr/src/shortnr/
--- shortnr/__init__.py
+++ shortnr/__init__.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 
 def create_app(test_config=None):
+    db = MongoEngine()
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
+        MONGODB_DB=os.environ.get('MONGODB_DB'),
+        MONGODB_USERNAME=os.environ.get('MONGODB_USERNAME'),
+        MONGODB_PASSWORD=os.environ.get('MONGODB_PASSWORD'),
     )
+    db.init_app(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -20,6 +26,6 @@ def create_app(test_config=None):
 
     @app.route('/hello')
     def hello():
-        return "Hello, World!"
+        return "Hello, World!!"
 
     return app
